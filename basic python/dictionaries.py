4e7af421a349b6fca9dd5d2f2009c99dcbdef9ed
Sadiqhasan Rupani <sadiqhasanrupani11@gmail.com>
2022-06-29 11:29:04 +0530

    .added the file of dictionaires.py and it's output.

---
+++ basic python/dictionaries.py
@@ -0,0 +1,13 @@
+# Dictionaries
+dic = {35: ['Sadiqhasan Rupani', 'Su Mangal Vastu near jawahar society', 'age', 19],
+       33: ['Soham Ganmote', 'xyz colony plot no.78', 'age', 19],
+       37: ['Yasin Bhojani', 'Hussaini Plaza near khoja colony', 'age', 20],
+       38: ['Zakir Kalwani','Khoja colony plot no.8', 'age', 20]}
+
+print('\n Roll numbers in my database: 33, 35, 37, 38' )
+
+data = int(input('\n What roll number data are you interested in?\n -> '))
+
+req_data = dic[data]
+
+print(f'\n What you were looking for is available here: \n -> {req_data} \n')
