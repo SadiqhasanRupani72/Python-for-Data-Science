8cc9db48c43182a326ee6c7c8dba2aae6c221b8f
Sadiqhasan Rupani <sadiqhasanrupani11@gmail.com>
2022-06-30 19:27:59 +0530

    .added and .modifided

---
+++ basic python/functions/map.py
@@ -0,0 +1,14 @@
+def sqr(x):
+	return x **2
+
+sqrlist = []
+
+n = int(input('Enter how many integer items you wanna add in you list: '))
+
+for i in range(0, n):
+	ele = int(input(f'Enter the integer item no {i+1}: '))
+	sqrlist.append(ele)
+
+sqr_map_list= list(map(sqr,sqrlist));
+
+print(f'\nThe square of the list{sqrlist} is: {sqr_map_list} \n')
