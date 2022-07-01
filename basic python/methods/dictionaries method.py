edb565944a5171633bb01dac3d71e2135b830fc3
Sadiqhasan Rupani <sadiqhasanrupani11@gmail.com>
2022-07-01 10:28:17 +0530

    .added and .modified

---
+++ basic python/methods/dictionaries method.py
@@ -0,0 +1,20 @@
+
+# Creating a dictionary
+dic = {'key_1': 'value1', 'key_2': 'value2', 35: 'Sadiqhasan Rupani', 37: 'Yasin Bhojani', 33: 'Soham Ganmote'}
+
+# Using operations on the 'dic' variable, which stores dictionaries.
+
+#First we display the dictionary.
+print(f'\n The default dic of this program is.\n -> {dic}')
+
+# key method: It returns the dictionary keys.
+keys = dic.keys()
+print(f'\n Keys of the dictionaries are. \n -> {keys}. ')
+
+# value method: It returns the dictionary values.
+values = dic.values()
+print(f'\n Values of the dictionaries are. \n -> {values}. ')
+
+# Item method: It returns the dictionary Items.
+items = dic.items()
+print((f'\n Items of the dictionaries are. \n -> {items}.  '))
---
+++ basic python/methods/methods_1.py
@@ -0,0 +1,11 @@
+# Creating a string.
+
+string = "My name is Sadiqhasan! #Sadiqhasan"
+
+print(f'\n The string is: {string}. \n')
+upr = string.upper()
+print(f' Upper casing the string: {upr}')
+
+split_ele = tuple(string.split('#'))
+
+print(f'\n spliting the string: {split_ele}\n')
